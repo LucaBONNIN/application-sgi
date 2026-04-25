40e4c60e733b25e0516c9f4eb75a7c580b856869
William Gérald Blondel <contact@williamblondel.fr>
2026-04-25 19:20:24 +0200

    feat: restrict AuditResource access to super_admin users only

--- app/Filament/App/Resources/AuditResource.php
+++ app/Filament/App/Resources/AuditResource.php
@@ -4,10 +4,16 @@ namespace App\Filament\App\Resources;
 
 use BackedEnum;
 use Filament\Support\Icons\Heroicon;
+use Illuminate\Support\Facades\Auth;
 use Tapp\FilamentAuditing\Filament\Resources\Audits\AuditResource as OriginalAuditResource;
 
 class AuditResource extends OriginalAuditResource
 {
+    public static function canAccess(): bool
+    {
+        return Auth::user()?->hasRole('super_admin') ?? false;
+    }
+
     public static function getNavigationGroup(): ?string
     {
         return __('filament/navigation.groups.admin');
