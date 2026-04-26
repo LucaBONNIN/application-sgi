324350453630e7f3f60f2ba2f15ff76067a977b1
William Gérald Blondel <contact@williamblondel.fr>
2026-04-26 17:50:45 +0200

    feat: redirect to index page after creating or updating a budget

--- app/Filament/App/Resources/Budgets/Pages/EditBudget.php
+++ app/Filament/App/Resources/Budgets/Pages/EditBudget.php
@@ -10,6 +10,11 @@ class EditBudget extends EditRecord
 {
     protected static string $resource = BudgetResource::class;
 
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
     protected function getHeaderActions(): array
     {
         return [
